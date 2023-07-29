dd6830150154151f3cd1353c495354c11123986c
Rdohero <rojaridho8888@gmail.com>
2023-07-29 19:03:50 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+
+
+
+
+<!-- sekolah -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $kelas = "11 PPLG 1";
+    ?>
+    <h1> <?php //echo "Ridho ! sekarang sudah naik ke kelas " . $kelas?></h1>
+</body>
+</html>
